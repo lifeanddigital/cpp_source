bf715f1cfdd25003602959a0ea8f50562c0c31d8
LifeandDigital <75499701+lifeanddigital@users.noreply.github.com>
2024-09-06 04:19:34 +0900

    Youtubr

--- pj0010/src0010/ex0010.cpp
+++ pj0010/src0010/ex0010.cpp
@@ -1,9 +1,10 @@
-
+/* 練習　ex0010.cpp */
+// ヘッダーをインクルード
 #include <iostream>
-
+// 標準(std) 名前空間
 using namespace std;
-
-int main()
-{
-    cout << "Hello Word \n" ;
-}
+//メイン関数
+int main() { //メイン関数開始
+    //コンソールに"Hello World!"を出力し、改行
+    cout << "Hello World! \n";
+} // 5. メイン関数の終了
